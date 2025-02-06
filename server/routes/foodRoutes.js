aae927b3fe863a76510fd11c5e9f768dcd619d8f
suvamneog <neogsuvam@gmail.com>
2025-02-06 21:18:09 +0530

    ADDED GET REQUEST

--- server/routes/foodRoutes.js
+++ server/routes/foodRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const Food = require("../models/food");
-const auth = require("../middleware/authMiddleware")
+const auth = require("../middleware/authMiddleware");
+const { findById } = require("../models/user");
 require("dotenv").config();
 
-
+//create food
 router.post("/", auth, async (req, res) => {
     let { name, calories, protein, carbs, fats } = req.body;
-    let getFood = new Food({
+    let addFood = new Food({
       name,
       calories,
       carbs,
@@ -15,9 +16,27 @@ router.post("/", auth, async (req, res) => {
       fats,
       user: req.user.id,
     });
-    await getFood.save();
-    console.log(getFood);
+    await addFood.save();
+    console.log(addFood);
     res.send("Food added!");
   });
   
+
+//get food
+router.get("/", auth, async (req, res) => {
+    let food = await Food.find({user : req.user.id});
+    console.log(food);
+    res.send("Food!");
+});
+
+//specific food
+router.get("/:id", auth, async (req, res) => {
+    let oneFood = await Food.findById(req.params.id);
+    if (!oneFood) return res.status(404).json({ message: "Food not found" });
+    console.log(oneFood);
+    res.send("Food!");
+})
+
+
+
   module.exports = router;
